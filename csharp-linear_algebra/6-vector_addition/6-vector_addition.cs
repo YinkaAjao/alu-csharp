d26f10979abe0b195a790a36b3b0ada0c9b2f2a0
YinkaAjao <160027843+YinkaAjao@users.noreply.github.com>
2026-03-11 10:28:14 +0200

    csharp-inheritance

--- csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -5,18 +5,18 @@
         if ((vector1.Length != 2 && vector1.Length != 3) ||
             (vector2.Length != 2 && vector2.Length != 3))
         {
-            return new double[] { -1 }; // Return [-1] if any vector is not 2D or 3D
+            return new double[] { -1 }; /// Return [-1] if any vector is not 2D or 3D
         }
 
         if (vector1.Length != vector2.Length)
         {
-            return new double[] { -1 }; // Return [-1] if vectors are not of the same size
+            return new double[] { -1 }; /// Return [-1] if vectors are not of the same size
         }
 
         double[] result = new double[vector1.Length];
         for (int i = 0; i < vector1.Length; i++)
         {
-            result[i] = vector1[i] + vector2[i]; // Add corresponding components
+            result[i] = vector1[i] + vector2[i]; /// Add corresponding components
         }
 
         return result;
